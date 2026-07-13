49aae312c6d9994198f34c889df1b3bbae25cc11
Tisa Shrestha <141940566+tisashrestha@users.noreply.github.com>
2026-07-13 12:07:48 +0545

    Update check_model.py

--- check_model.py
+++ check_model.py
@@ -1,15 +1,14 @@
 from ultralytics import YOLO
 import os
 
-# Update this path to where your file actually is
-model_path = "weights/bestmodel.pt" 
+model_path = "weights/bestaug.pt" 
 
 if os.path.exists(model_path):
     try:
         model = YOLO(model_path)
-        print("✅ Success! Model loaded correctly.")
+        print("Success! Model loaded correctly.")
         print("Detected Classes:", model.names) 
     except Exception as e:
-        print(f"❌ Error loading model: {e}")
+        print(f"Error loading model: {e}")
 else:
-    print(f"❌ File not found at {model_path}. Check your folder structure!")
+    print(f"File not found at {model_path}. Check your folder structure!")
